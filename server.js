a08642ee1089e8e47cf9c626b05c4004f0d4ab65
Josh Coyco <jac6308@gmail.com>
2020-02-11 17:53:26 -0800

    Commented package.json

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+//import libraries
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-
+//Import routers
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
 
+//Setup Views
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
@@ -21,6 +23,7 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 app.use(methodOverride('_method'))
 
+//Setup mongoose server
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { 
     useNewUrlParser: true})
@@ -28,6 +31,7 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+//Setup Routers
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
